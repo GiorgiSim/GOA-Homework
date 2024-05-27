c1081d0c1034f79754015ea98662296d8eb72b5d
GiorgiSim <simonishvili.g12@gmail.com>
2024-05-28 01:03:00 +0400

    second commit

---
+++ Level 03/homework/homework2.py
@@ -0,0 +1,82 @@
+name = "giorgi"
+
+last_name = "simonishvili"
+
+age = 13
+
+hobby = "football"
+
+country = "georgia"
+
+print (name, last_name, age, hobby, country )
+
+
+x = 24
+y = 42
+a = 12
+
+print (x + y + a)
+
+x = 223
+y = 554
+a = 432
+b = 2415
+
+print (x, y, a, b) 
+
+print (20 + 15)
+print (122 + 56)
+print (78 + 33)
+print (66 + 15)
+print (44 + 21)
+print (32 + 11)
+print (54 + 23)
+print (29 + 18)
+print (22 + 33)
+print (96 + 100)
+print (54 + 355)
+
+print (24 - 9)
+print (55 - 5)
+print (21 - 4)
+print (15 - 2)
+print (15 - 3)
+print (23 - 14)
+print (14 - 8)
+print (24 - 16)
+print (45 - 20)
+print (90 - 72)
+print (365 - 295)
+
+print (12 * 24)
+print (24 * 6)
+print (5 * 5)
+print (6 * 13)
+print (8 * 3)
+print (7 * 2)
+print (9 * 2)
+print (10 * 10)
+print (3 * 4)
+print (8 * 5)
+
+print (10 // 2)
+print (4 // 2)
+print (25 // 5)
+print (40 // 5)
+print (2 // 2)
+print (27 // 3)
+print (28 // 4)
+print (60 // 5)
+print (40 // 4)
+print (4 // 4)
+
+print (5 ** 5)
+print (10 ** 5)
+print (40 ** 2)
+print (20 ** 5)
+print (30 ** 5)
+print (60 ** 5)
+print (50 ** 5)
+print (70 ** 5)
+print (40 ** 5)
+print (30 ** 6)
