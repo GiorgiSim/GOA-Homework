0bc4f57ecf407835fcf6b48f777ebdbf6c7649e2
GiorgiSim <simonishvili.g12@gmail.com>
2024-06-15 00:00:01 +0400

    second commit

--- Level 01/homework/homework1.py
+++ Level 01/homework/homework1.py
@@ -1,2 +1,42 @@
-print("Giorgi Simonishvili")
-print("this is day 1")
+name = "Giorgi"
+# name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+# "nika" არის ცვლადისთვის მნიშნელობა
+
+surname = "Simonishvili"
+
+#print( name )
+#პრინტ ფუნქცია გადაეცემა ეკრანზე გასატანი ობიექტი
+
+name = "Giorgi" # ეს არის str ( string ) ტიპის ცვლადი
+age = 13 # ეს არის int ( integer ) მთელი რიცხვი
+height = 162.5 #ეს არის float ტიპის ცვლადი ( ათწილადი )
+# bulean (bool) ტიპის ცვლადი
+
+knows_programming = True
+is_ugly = False
+
+isUgly = False
+
+
+#print(name + " " + surname)
+
+#სტრინგი არის პრჭყალებში მოქცეული სიმბოლოები
+#print(name + age)
+
+#print(type(age))
+#print(type(name))
+#print(type(surname))
+#print(type(height))
+#print(type(knows_programming))
+
+print(name +" "+ str(age))
+
+me = "me mqvia"
+my = "me var"
+mu = "simagleshi var"
+mi = "wlis"
+nm = "programirobashi normaluri var"
+mm = "miyvars manqanis tareba"
+
+print (me +" "+ name +" "+ my +" "+ str(age) +" "+ mi +" "+ mu +" "+ str(height) +" "+ nm +" "+ mm)
